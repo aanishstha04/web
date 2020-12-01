c762a53c9a0b4a28e43108520d613ae9c6700b1c
aanishstha04 <aanishstha04@gmail.com>
2020-11-30 19:27:43 -0800

    Initial commit

---
+++ back-end/src/index.server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const env = require('dotenv');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//routes
+const userRoutes = require('./routes/user');
+
+
+//enviroment variable
+env.config();
+
+//mongodb connection 
+//mongodb+srv://admin:<password>@rhynocity.mrdby.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@rhynocity.mrdby.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+).then(() =>{
+    console.log('database connected');
+});
+
+
+app.use(bodyParser());
+app.use('/api', userRoutes);
+
+
+app.listen(process.env.PORT, () =>{
+    console.log(`server is running at port ${process.env.PORT}`);
+});
