fa3f510c734f1ae7a39cbffe15c90a6a55372b2e
aanishstha04 <aanishstha04@gmail.com>
2020-12-01 20:53:52 -0800

    Full signin/signup auth

---
+++ src/routes/admin/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { signup, signin, requireSignin } = require('../../controller/admin/user');
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../../validators/auth');
+const router = express.Router();
+
+
+router.post('/admin/signup', validateSignupRequest, isRequestValidated,signup);
+
+router.post('/admin/signin', validateSigninRequest,isRequestValidated ,signin);
+
+module.exports = router;
